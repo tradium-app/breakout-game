1328b4004c15b71b385278063772b1ba5ffb2444
Suraj Shrestha <syuraj@gmail.com>
2021-06-20 03:42:39 -0400

    some refactoring: adding usesCleartextTraffic

--- android/app/src/main/java/com/tradiumapp/breakout/qa/MainActivity.java
+++ android/app/src/main/java/com/tradiumapp/breakout/qa/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package com.tradiumapp.breakout.qa;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'breakout-game',
-  webDir: 'build',
-  bundledWebRuntime: false
+  appId: "com.tradiumapp.breakout.qa",
+  appName: "breakout-game",
+  webDir: "build",
+  bundledWebRuntime: false,
 };
 
 export default config;
