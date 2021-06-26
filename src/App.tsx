9a23fe11ad716b33f93c548e4791592e39cb30a5
Suraj Shrestha <syuraj@gmail.com>
2021-06-26 15:42:10 -0400

    configuring google analytics using ionic plugins

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,59 @@
-import { Redirect, Route } from "react-router-dom";
-import { ApolloProvider } from "@apollo/client/react";
-import { IonApp, IonRouterOutlet } from "@ionic/react";
-import { IonReactRouter } from "@ionic/react-router";
-import Home from "./pages/Home";
-import graphqlClient from "./graphql-client";
+import { Redirect, Route } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client/react';
+import { IonApp, IonRouterOutlet } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import Home from './pages/Home';
+import graphqlClient from './graphql-client';
+import { GoogleAnalytics } from '@ionic-native/google-analytics';
 
 /* Core CSS required for Ionic components to work properly */
-import "@ionic/react/css/core.css";
+import '@ionic/react/css/core.css';
 
 /* Basic CSS for apps built with Ionic */
-import "@ionic/react/css/normalize.css";
-import "@ionic/react/css/structure.css";
-import "@ionic/react/css/typography.css";
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
 
 /* Optional CSS utils that can be commented out */
-import "@ionic/react/css/padding.css";
-import "@ionic/react/css/float-elements.css";
-import "@ionic/react/css/text-alignment.css";
-import "@ionic/react/css/text-transformation.css";
-import "@ionic/react/css/flex-utils.css";
-import "@ionic/react/css/display.css";
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
 
 /* Theme variables */
-import "./theme/variables.css";
+import './theme/variables.css';
+import { useEffect } from 'react';
 
-const App: React.FC = () => (
-  <ApolloProvider client={graphqlClient}>
-    <IonApp>
-      <IonReactRouter>
-        <IonRouterOutlet>
-          <Route exact path="/home">
-            <Home />
-          </Route>
-          <Route exact path="/">
-            <Redirect to="/home" />
-          </Route>
-        </IonRouterOutlet>
-      </IonReactRouter>
-    </IonApp>
-  </ApolloProvider>
-);
+const App: React.FC = () => {
+  useEffect(() => {
+    GoogleAnalytics.startTrackerWithId(process.env.REACT_APP_MEASUREMENTID)
+      .then(() => {
+        console.log('Google analytics is ready now');
+        GoogleAnalytics.trackView('test');
+        // Tracker is ready
+        // You can now track pages or set additional information such as AppVersion or UserId
+      })
+      .catch(e => console.log('Error starting GoogleAnalytics', e));
+  }, []);
+
+  return (
+    <ApolloProvider client={graphqlClient}>
+      <IonApp>
+        <IonReactRouter>
+          <IonRouterOutlet>
+            <Route exact path="/home">
+              <Home />
+            </Route>
+            <Route exact path="/">
+              <Redirect to="/home" />
+            </Route>
+          </IonRouterOutlet>
+        </IonReactRouter>
+      </IonApp>
+    </ApolloProvider>
+  );
+};
 
 export default App;
