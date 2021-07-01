f8de3f826dc6e5730b910f08c43975085e0c4d41
Suraj Shrestha <syuraj@gmail.com>
2021-07-01 00:16:22 -0400

    adding github action file for google play publish

---
+++ prepare_release.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+PACKAGE_NAME="com.tradiumapp.breakout"
+APP_NAME="BreakOut Game"
+TIME_STAMP=$(date +%s)
+IC_LAUNCHER_BACKGROUND="#E7F3FF"
+BUILD_GRADLE=./android/app/build.gradle
+ANDROID_STRINGS_FILE=./android/app/src/main/res/values/strings.xml
+IC_LAUNCHER_BACKGROUND_FILE=./android/app/src/main/res/values/ic_launcher_background.xml
+
+sed -i '' -e 's/applicationId "[^"]*"/applicationId "'$PACKAGE_NAME'"/' $BUILD_GRADLE
+sed -i '' -e 's/versionCode .*$/versionCode '$TIME_STAMP'/' $BUILD_GRADLE
+sed -i '' -e 's/"app_name">[^<]*</"app_name">'$APP_NAME'</' $ANDROID_STRINGS_FILE
+
+sed -i '' -e 's/"ic_launcher_background">[^<]*</"ic_launcher_background">'$IC_LAUNCHER_BACKGROUND'</' $IC_LAUNCHER_BACKGROUND_FILE
+
+mv .env.release .env
+mv ./android/app/google-services.json.prod ./android/app/google-services.json
